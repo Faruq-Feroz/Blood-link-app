2ea9a43f1b775ad1ffecce399313572720c2c50d
Faruq-Feroz <164517766+Faruq-Feroz@users.noreply.github.com>
2024-11-12 05:27:28 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@
 const Header = () => {
     return (
         <header className="app-header">
-            <img src="https://damu-link.vercel.app/Logo.png" alt="App Logo" className="app-logo" />
+            <img src="https://blood-link-ecru.vercel.app/Logo.png" alt="App Logo" className="app-logo" />
             <h1 className="app-title">SupportSync</h1>
         </header>
     );
